1949f74341f137993a5bab0cb7df2ec57bc2b58f
Sri Harsha Hari <sriharshahari@yahoo.com>
2022-06-01 11:37:39 +0530

    add grid function added

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 const slider=document.getElementById("slider");
 const sliderValue=document.getElementById("sliderValue");
-const mainGrid=document.getElementById("container");
+const container=document.getElementById("container");
+let baseDiv;
+let newGrid;
+
+//function to show the grid value selected via the slider
+sliderValue.textContent="2x2";
+slider.onchange=function(){
+    container.style.setProperty('grid-template-columns', 'repeat(' + slider.value + ', 1fr)');
+    sliderValue.textContent=this.value + 'x' + this.value;
+    document.querySelectorAll('.baseElements').forEach(f => f.remove());
+    document.querySelectorAll('.newElements').forEach(e => e.remove());
+    addGrid();
+}
+
+container.style.setProperty('grid-template-columns', 'repeat(2, 1fr)');
+for(i=1;i<=2;i++){
+    for(j=1;j<=2;j++){
+        baseDiv=document.createElement("div");
+        baseDiv.className="baseElements";
+        container.appendChild(baseDiv);
+
+    }
+}
+
+//function to create grid
 
 function addGrid() {
-    let newGrid=document.createElement(div);
-    newGrid.classList.add()
+    for(let i=1;i<=slider.value;i++){
+        for(let j=1;j<=slider.value;j++){
+            newGrid=document.createElement("div");
+            newGrid.className="newElements";
+            container.appendChild(newGrid);
+        }
+    }
 }
