a173334487674bf8347db5acd17789bf3e152152
Sri Harsha Hari <sriharshahari@yahoo.com>
2022-06-01 10:17:39 +0530

    created a grid

---
+++ script.js
@@ -0,0 +1,8 @@
+const slider=document.getElementById("slider");
+const sliderValue=document.getElementById("sliderValue");
+const mainGrid=document.getElementById("container");
+
+function addGrid() {
+    let newGrid=document.createElement(div);
+    newGrid.classList.add()
+}
